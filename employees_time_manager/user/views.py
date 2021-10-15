ff3d3c79aaba9fdc4a2182348eae635a6ce1c8bd
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-10-15 13:03:36 +0200

    'register'

---
+++ employees_time_manager/user/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+
+def login(request):
+
+    if request.method == 'POST':
+        print(request.POST)
+
+    return render(request, 'user/login_register.html', {'form':'login'})
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            user = None
+
+        if not user:
+            if password1 == password2:
+                # create new user
+                print(username, password2)
+                NewUser = User()
+                NewUser.username = username
+                NewUser.password = password1
+                NewUser.save()
+                return redirect('login')
+            else:
+                # display not matching password message
+                None
+        else:
+            #display taken username message
+            None
+            
+
+
+
+    return render(request, 'user/login_register.html', {'form':'register'})
